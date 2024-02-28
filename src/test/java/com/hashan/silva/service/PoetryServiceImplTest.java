8ee5d462a6af569a3eaf069ca54a83692dfdf505
hashan.silva <hashan.silva@gmail.com>
2024-02-28 23:02:01 +0530

    HA:ENCH:changing spring ai dependencies

--- src/test/java/com/hashan/silva/service/PoetryServiceImplTest.java
+++ src/test/java/com/hashan/silva/service/PoetryServiceImplTest.java
@@ -1,26 +1,23 @@
 package com.hashan.silva.service;
 
 import com.hashan.silva.model.Poetry;
-import com.hashan.silva.service.PoetryService;
 import com.hashan.silva.service.impl.PoetryServiceImpl;
 import com.hashan.silva.util.Constant;
 import org.apache.commons.lang3.NotImplementedException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.springframework.ai.client.AiClient;
-import org.springframework.ai.client.AiResponse;
-import org.springframework.ai.client.Generation;
-import org.springframework.ai.prompt.Prompt;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.Matchers.containsStringIgnoringCase;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
@@ -28,7 +25,7 @@ import static org.mockito.Mockito.*;
 public class PoetryServiceImplTest {
 
     @Mock
-    private AiClient aiClient;
+    private OpenAiChatClient aiClient;
 
     @Test
     public void testGenerateHaiku_Exception() {
@@ -46,11 +43,11 @@ public class PoetryServiceImplTest {
 
     @Test
     public void testGenerateHaiku() {
-        when(aiClient.generate(Constant.WRITE_ME_HAIKU_ABOUT_CAT)).thenReturn("Haiku about cat");
+        when(aiClient.call(Constant.WRITE_ME_HAIKU_ABOUT_CAT)).thenReturn("Haiku about cat");
         PoetryService poetryService = new PoetryServiceImpl(aiClient);
         String response = poetryService.generateHaiku();
         assertEquals("Haiku about cat", response);
-        verify(aiClient).generate(Constant.WRITE_ME_HAIKU_ABOUT_CAT);
+        verify(aiClient).call(Constant.WRITE_ME_HAIKU_ABOUT_CAT);
     }
 
     @Test
@@ -63,8 +60,8 @@ public class PoetryServiceImplTest {
                 "}";
         List<Generation> generations = new ArrayList<>();
         generations.add(new Generation(generatedPoetryString));
-        AiResponse aiResponse = new AiResponse(generations);
-        when(aiClient.generate((Prompt) any())).thenReturn(aiResponse);
+        ChatResponse aiResponse = new ChatResponse(generations);
+        when(aiClient.call((Prompt) any())).thenReturn(aiResponse);
 
         PoetryService poetryService = new PoetryServiceImpl(aiClient);
         Poetry generatedPoetry = poetryService.generatePoetry("genre", "theme");
